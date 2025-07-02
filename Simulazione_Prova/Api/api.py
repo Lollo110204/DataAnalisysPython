652c12670eea1d2fe6ed87958f7763804dd7a686
Lorenzo <lorenzo.prette@edu.itspiemonte.it>
2025-07-02 23:41:23 +0200

    readme per le info sulla documentazione esame pratico

--- Simulazione_Prova/Api/api.py
+++ Simulazione_Prova/Api/api.py
@@ -14,19 +14,83 @@ app = FastAPI(
 )
 
 def query_db(query:str,params: tuple = ()):
-    conn = sqlite3.connect("prova.db")
+    conn = sqlite3.connect("prova_esame.db")
     df = pd.read_sql_query(query,conn,params=params)
     conn.close()
     return df
 
 
-@app.get("/getProva")
+@app.get("/importanza")
 def prova_get(da_anno:Optional[int]=None, a_anno:Optional[int]=None):
-    query = "SELECT geo,TIME_PERIOD, OBS_VALUE ,species FROM prova "
+    query = "SELECT *  FROM importanza_pesca "
     params=[]
     if da_anno and a_anno:
-        query += "WHERE TIME_PERIOD BETWEEN ? and ?"
+        query += "WHERE anno BETWEEN ? and ?"
         params.extend([da_anno,a_anno])
-        
+
     df = query_db(query,tuple(params))
     return df.to_dict(orient='records')
+
+@app.get("/occupazione")
+def prova_get(da_anno:Optional[int]=None, a_anno:Optional[int]=None):
+    query = "SELECT *  FROM occupazione_pesca "
+    params=[]
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? and ?"
+        params.extend([da_anno,a_anno])
+
+    df = query_db(query,tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/produttivita")
+def prova_get(da_anno:Optional[int]=None, a_anno:Optional[int]=None):
+    query = "SELECT *  FROM produttivita_pesca "
+    params=[]
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? and ?"
+        params.extend([da_anno,a_anno])
+
+    df = query_db(query,tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/importanza_media_percentuale")
+def prova_get(da_anno:Optional[int]=None, a_anno:Optional[int]=None):
+    query = "SELECT *  FROM importanza_media_percentuale "
+    params=[]
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? and ?"
+        params.extend([da_anno,a_anno])
+
+    df = query_db(query,tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/media_occupazione_aree_geografiche")
+def prova_get(da_anno:Optional[int]=None, a_anno:Optional[int]=None):
+    query = "SELECT *  FROM media_occupazione_aree_geografiche "
+    params=[]
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? and ?"
+        params.extend([da_anno,a_anno])
+
+    df = query_db(query,tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/produttiva_totale_anno")
+def prova_get(da_anno:Optional[int]=None, a_anno:Optional[int]=None):
+    query = "SELECT *  FROM produttiva_totale_anno "
+    params=[]
+    if da_anno and a_anno:
+        query += "WHERE anno BETWEEN ? and ?"
+        params.extend([da_anno,a_anno])
+
+    df = query_db(query,tuple(params))
+    return df.to_dict(orient='records')
+
+@app.get("/regioni")
+def prova_get():
+    query = "SELECT *  FROM regioni"
+    params=[]
+
+    df = query_db(query,tuple(params))
+    return df.to_dict(orient='records')
+
